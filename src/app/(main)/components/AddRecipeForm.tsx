47b4f2dce2cb0df3369145756ec0b5fee8c9d08a
Ryan <ryanmilkiewicz@gmail.com>
2026-04-06 09:50:09 -0400

    added more validation

--- src/app/(main)/components/AddRecipeForm.tsx
+++ src/app/(main)/components/AddRecipeForm.tsx
@@ -3,6 +3,15 @@ import { Field, FieldGroup, FieldLabel } from "@/components/ui/field";
 import { IngredientField } from "./IngredientField";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectLabel,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
 import { StepField } from "./StepField";
 import { Textarea } from "@/components/ui/textarea";
 import { useForm } from "@tanstack/react-form";
@@ -15,6 +24,10 @@ const formSchema = z.object({
     .min(5, "Recipe Description must be at least 5 characters."),
   prepTime: z.string().min(1, "Prep Time must be at least 1 character"),
   cookTime: z.string().min(1, "Cook Time must be at least 1 character"),
+  servings: z.string().trim().min(1, "Servings must be at least 1 character"),
+  difficulty: z.enum(["easy", "medium", "hard"], {
+    message: "Please select a difficulty level",
+  }),
 });
 
 export default function AddRecipeForm() {
@@ -24,9 +37,12 @@ export default function AddRecipeForm() {
       recipeDescription: "",
       prepTime: "",
       cookTime: "",
+      servings: "",
+      difficulty: "" as "easy" | "medium" | "hard",
     },
     validators: {
       onSubmit: formSchema,
+      onChange: formSchema,
     },
     onSubmit: async ({ value }) => {
       // TODO: add toasts for success
@@ -122,32 +138,96 @@ export default function AddRecipeForm() {
                 </Field>
               )}
             </form.Field>
-            {/* <Field className="flex-1">
-              <FieldLabel htmlFor="cookTime">Cook Time</FieldLabel>
-              <Input id="cookTime" placeholder="Cook Time" />
-            </Field> */}
           </div>
           <div className="flex gap-4">
-            <Field className="flex-1">
-              <FieldLabel htmlFor="servings">Servings</FieldLabel>
-              <Input id="servings" placeholder="4" />
-            </Field>
-            <Field className="flex-1">
-              <FieldLabel htmlFor="difficulty">Difficulty</FieldLabel>
-              <Input id="difficulty" placeholder="Medium" />
-            </Field>
+            <form.Field name="servings">
+              {(field) => (
+                <Field className="flex-1">
+                  <FieldLabel htmlFor="PrepTime">Servings</FieldLabel>
+                  <Input
+                    id="servings"
+                    placeholder="Servings"
+                    value={field.state.value}
+                    onBlur={field.handleBlur}
+                    onChange={(e) => field.handleChange(e.target.value)}
+                  />
+                  {field.state.meta.errors.length > 0 && (
+                    <p className="text-sm text-destructive">
+                      {field.state.meta.errors[0]?.message}
+                    </p>
+                  )}
+                  {/* {field.state.meta.errors.length > 0 && (
+                    <p className="text-sm text-destructive">
+                      {typeof field.state.meta.errors[0] === "string"
+                        ? field.state.meta.errors[0]
+                        : field.state.meta.errors[0]?.message}
+                    </p>
+                  )} */}
+                </Field>
+              )}
+            </form.Field>
+            <form.Field name="difficulty">
+              {(field) => (
+                <Field className="flex-1">
+                  <FieldLabel htmlFor="PrepTime">Difficulty</FieldLabel>
+                  <Select
+                    value={field.state.value}
+                    onValueChange={(value) =>
+                      field.handleChange(value as "easy" | "medium" | "hard")
+                    }
+                  >
+                    <SelectTrigger className="w-full">
+                      <SelectValue placeholder="Select a fruit" />
+                    </SelectTrigger>
+                    <SelectContent>
+                      <SelectGroup>
+                        <SelectLabel>Difficulty</SelectLabel>
+                        <SelectItem value="easy">Easy</SelectItem>
+                        <SelectItem value="medium">Medium</SelectItem>
+                        <SelectItem value="hard">Hard</SelectItem>
+                      </SelectGroup>
+                    </SelectContent>
+                  </Select>
+                  {field.state.meta.errors.length > 0 && (
+                    <p className="text-sm text-destructive">
+                      {field.state.meta.errors[0]?.message}
+                    </p>
+                  )}
+                </Field>
+              )}
+            </form.Field>
           </div>
-          <Field>
-            <FieldLabel htmlFor="tags">Tags</FieldLabel>
-            <Input id="tags" placeholder="Italian, weeknight, pasta..." />
-          </Field>
+          {/* <form.Field name="tags">
+            {(field) => (
+              <Field>
+                <FieldLabel htmlFor="tags">Tags</FieldLabel>
+                <Input
+                  id="tags"
+                  placeholder="Italian, weeknight, pasta..."
+                  value={field.state.value}
+                  onBlur={field.handleBlur}
+                  onChange={(e) => field.handleChange(e.target.value)}
+                />
+                {field.state.meta.errors.length > 0 && (
+                  <p className="text-sm text-destructive">
+                    {field.state.meta.errors[0]?.message}
+                  </p>
+                )}
+              </Field>
+            )}
+          </form.Field> */}
         </FieldGroup>
         <Separator className="my-6" />
         <IngredientField />
         <Separator className="my-6" />
         <StepField />
         <Separator className="my-6" />
-        <Button className="w-40 rounded-full" variant="outline" size="default">
+        <Button
+          type="submit"
+          className="w-40 rounded-full"
+          variant="outline"
+          size="default"
+        >
           Submit Recipe
         </Button>
       </form>
