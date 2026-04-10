c5b1b63237e145d250bc7f92a9404434ce81d2bc
Ryan <ryanmilkiewicz@gmail.com>
2026-04-10 12:38:58 -0400

    show no recipes found text

--- src/app/(main)/recipes/page.tsx
+++ src/app/(main)/recipes/page.tsx
@@ -13,18 +13,24 @@ export default async function Page({
 
   return (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-3 p-6">
-      {recipes.map((recipe) => (
-        <RecipeCard
-          key={recipe.id}
-          id={recipe.id}
-          title={recipe.title}
-          description={recipe.description}
-          cookTime={recipe.cookTime}
-          servings={recipe.servings}
-          imageUrl={recipe.imageUrl ? recipe.imageUrl : ""}
-          tags={recipe.recipeTags.map((rt) => rt.tag)}
-        />
-      ))}
+      {recipes && recipes.length > 0 ? (
+        recipes.map((recipe) => (
+          <RecipeCard
+            key={recipe.id}
+            id={recipe.id}
+            title={recipe.title}
+            description={recipe.description}
+            cookTime={recipe.cookTime}
+            servings={recipe.servings}
+            imageUrl={recipe.imageUrl ? recipe.imageUrl : ""}
+            tags={recipe.recipeTags.map((rt) => rt.tag)}
+          />
+        ))
+      ) : (
+        <p className="text-center text-muted-foreground col-span-full">
+          No recipes found.
+        </p>
+      )}
     </div>
   );
 }
