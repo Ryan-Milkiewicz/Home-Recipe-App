ad7e123d6b321e8a783b9f6bcc2a87112dbcf478
Ryan <ryanmilkiewicz@gmail.com>
2026-04-12 10:15:42 -0400

    fixed: config for drizzle

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,5 +1,8 @@
-import "dotenv/config";
+import * as dotenv from "dotenv";
+dotenv.config({ path: ".env.local" });
+
 import { defineConfig } from "drizzle-kit";
+
 export default defineConfig({
   out: "./drizzle",
   schema: "./src/db/schema.ts",
